8e537b531cbe780678d759217752f83a267da9a7
Uriel <uri_mysler@hotmail.com>
2015-03-03 13:22:26 -0300

    Agregado belongs to al regalo

--- grails-app/domain/meli/regalos/Regalo.groovy
+++ grails-app/domain/meli/regalos/Regalo.groovy
@@ -3,7 +3,7 @@ package meli.regalos
 class Regalo {
 	String url ;
 	Date fechaDeAsignacion = new Date();
-	/*static belongsTo= [empleado:Empleado];*/
+	static belongsTo= [empleado:Empleado];
 	static constraints = {
 	}
 	def void setUrl(String miUrl){
