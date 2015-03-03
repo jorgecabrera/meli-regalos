86135fb07f88c8be461a9bae02350934b257185c
Uriel <uri_mysler@hotmail.com>
2015-03-03 16:04:39 -0300

    Agregado la carpeta src al repo, incluye ItemML

---
+++ src/groovy/ItemML.groovy
@@ -0,0 +1,13 @@
+import com.google.gson.JsonObject
+
+
+class ItemML {
+	String descripcion;
+	String imagen;
+	String url;
+	public ItemML(JsonObject json){
+		descripcion=json.getAsString("title");
+		imagen=json.getAsString("thumbnail");
+		url=json.getAsString("permalink");
+	}
+}
