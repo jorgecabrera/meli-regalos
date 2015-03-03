5e27761c2d9946b7b16713e75e5782e85a3cb8ab
jorgecabrera <jorge.cabrera@mercadolibre.com>
2015-03-03 12:27:08 -0300

    Creo la clase regalo.

---
+++ grails-app/domain/meli/regalos/Regalo.groovy
@@ -0,0 +1,12 @@
+package meli.regalos
+
+class Regalo {
+	String url ;
+	Date fechaDeAsignacion = new Date();
+	/*static belongsTo= [empleado:Empleado];*/
+	static constraints = {
+	}
+	def void setUrl(String miUrl){
+		url = miUrl;
+	}
+}
